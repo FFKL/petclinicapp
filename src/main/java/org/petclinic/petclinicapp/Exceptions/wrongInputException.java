71fe2804c962cf98817e7488257e2372eccdd198
dmitriy <FFKL@yandex.ru>
2015-12-07 16:03:51 +0200

    WrongInput

---
+++ src/main/java/org/petclinic/petclinicapp/Exceptions/wrongInputException.java
@@ -0,0 +1,7 @@
+package org.petclinic.petclinicapp.Exceptions;
+
+public class WrongInputException extends Exception {
+    public WrongInputException(final String message) {
+        super(message);
+    }
+}
