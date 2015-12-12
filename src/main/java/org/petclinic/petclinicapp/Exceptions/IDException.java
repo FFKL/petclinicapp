b5c6d19ac5a49d5e35420335e9d419ae736b5ab6
dmitriy <FFKL@yandex.ru>
2015-12-12 20:20:59 +0200

    add IDException and PetTypeException

---
+++ src/main/java/org/petclinic/petclinicapp/Exceptions/IDException.java
@@ -0,0 +1,7 @@
+package org.petclinic.petclinicapp.Exceptions;
+
+public class IDException extends Exception {
+    public IDException(final String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/petclinic/petclinicapp/Exceptions/PetTypeException.java
@@ -0,0 +1,7 @@
+package org.petclinic.petclinicapp.Exceptions;
+
+public class PetTypeException extends Exception {
+    public PetTypeException(final String message) {
+        super(message);
+    }
+}
